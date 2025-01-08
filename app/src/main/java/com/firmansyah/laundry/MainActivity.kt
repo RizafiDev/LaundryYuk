8a13fc25d0dc881a2cf7c24f182ef4bd5c7d11b6
rizafidev <firmansyah.riza6@smk.belajar.id>
2025-01-08 14:10:45 +0700

    add: feature card

--- app/src/main/java/com/firmansyah/laundry/MainActivity.kt
+++ app/src/main/java/com/firmansyah/laundry/MainActivity.kt
@@ -5,10 +5,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.TextView
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val dateTextView: TextView = findViewById(R.id.date)
+        val currentDate = SimpleDateFormat("dd MMMM yyyy", Locale.getDefault()).format(Date())
+        dateTextView.text = currentDate
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
