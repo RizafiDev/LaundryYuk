2cb585d835603fa47e8146d990ab40f3efb9d82b
rizafidev <firmansyah.riza6@smk.belajar.id>
2025-01-24 08:18:35 +0700

    add: dynamic date

--- app/src/main/java/com/firmansyah/laundry/MainActivity.kt
+++ app/src/main/java/com/firmansyah/laundry/MainActivity.kt
@@ -17,7 +17,6 @@ class MainActivity : AppCompatActivity() {
         val currentDate = SimpleDateFormat("dd MMMM yyyy", Locale.getDefault()).format(Date())
         dateTextView.text = currentDate
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
